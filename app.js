01d2c2812bdd91c2dda3a5a8893da889900c6cc4
nikhillade-dev <nikhillade2017@gmail.com>
2024-11-27 12:41:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,3 +30,15 @@ window.addEventListener("scroll", (event) => {
         }
     })
 });  
+
+
+const button = document.querySelector('#button');
+const canvas = document.querySelector('#confetti');
+
+const jsConfetti = new JSConfetti();
+
+button.addEventListener('click', () => {
+    jsConfetti.addConfetti({
+        emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'],
+    }).then(() => jsConfetti.addConfetti())
+})
